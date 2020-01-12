23c217d6c162978d190b2aa9a7ff89429007b521
Nick Ferguson <ncf423@gmail.com>
2020-01-12 15:41:44 -0500

    add build file

---
+++ DashApp/build.zsh
@@ -0,0 +1,19 @@
+#!/usr/local/bin/zsh
+#
+# Use cheatset tool to generate Dash! docsets for all files
+
+if [ $# -eq 1 ]; then
+	BLDOUT=$1
+else
+	BLDOUT=./output
+fi
+
+if [ ! -d $BLDOUT ]; then
+	mkdir $BLDOUT
+fi
+
+for file in ./rbfiles/*.rb(.);
+do
+	cheatset generate $file
+	mv ./*.docset $BLDOUT/.
+done
